01b5f9867d82a890ad44ecdc3bcfb1bfb4e869be
dawidcieszewski <dawidcieszewski@gmail.com>
2021-05-15 15:04:24 +0200

    gradle setup

---
+++ app/src/main/java/com/fiesta/getrepoinfo/MainActivity.kt
@@ -0,0 +1,10 @@
+package com.fiesta.getrepoinfo
+
+class MainActivity : AppCompatActivity() {
+  
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
